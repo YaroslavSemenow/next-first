2cbe19115391ab7aeb5150c6208edef9e12e168f
Yaroslav Semenov <87042339+YaroslavSemenow@users.noreply.github.com>
2025-07-16 01:04:09 +0300

    move data to page

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -3,18 +3,23 @@ import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
 import {notFound} from "next/navigation";
-import {items} from "@/data/portfolio/portfolio";
+import {items} from "@/app/portfolio/[category]/data";
 
+const getData = (cat: string) => {
+    const data = items[cat];
+
+    return data ? data : notFound();
+};
 
 const Category = async ({params}: { params: { category: string } }) => {
-    const data = items[params.category];
+    const data = getData(params.category);
 
     return (
         <div className={styles.container}>
             <h1 className={styles.catTitle}>{params.category}</h1>
 
             {data?.map((item: any) => (
-                <div className={styles.item} key={item.id}>
+                <div className={styles.item} key={item._id}>
                     <div className={styles.content}>
                         <h1 className={styles.title}>{item.title}</h1>
                         <p className={styles.desc}>{item.desc}</p>
